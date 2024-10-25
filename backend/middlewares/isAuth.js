2844b32785265ed0382397f5833b60accd6e8ada
Okpah Evans Chukwunnaemeka <okpahevans@gmail.com>
2024-10-25 04:48:47 -0700

    Update isAuth.js

--- backend/middlewares/isAuth.js
+++ backend/middlewares/isAuth.js
@@ -2,27 +2,27 @@ import jwt from "jsonwebtoken";
 import { User } from "../models/User.js";
 
 export const isAuth = async (req, res, next) => {
-	  try {
-		      const token = req.cookies.token;
+  try {
+    const token = req.cookies.token;
 
-		      if (!token)
-			        return res.status(403).json({
-					        message: "Please Login",
-					      });
+    if (!token)
+      return res.status(403).json({
+        message: "Please Login",
+      });
 
-		      const decodedData = jwt.verify(token, process.env.Jwt_secret);
+    const decodedData = jwt.verify(token, process.env.Jwt_secret);
 
-		      if (!decodedData)
-			        return res.status(403).json({
-					        message: "token expired",
-					      });
+    if (!decodedData)
+      return res.status(403).json({
+        message: "token expired",
+      });
 
-		      req.user = await User.findById(decodedData.id);
+    req.user = await User.findById(decodedData.id);
 
-		      next();
-		    } catch (error) {
-			        res.status(500).json({
-					      message: "Please Login",
-					    });
-			      }
+    next();
+  } catch (error) {
+    res.status(500).json({
+      message: "Please Login",
+    });
+  }
 };
