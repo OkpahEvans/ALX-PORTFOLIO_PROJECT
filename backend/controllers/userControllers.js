8a9e50bf7dcf33522b441b9cdcf72fe49123e4f7
Okpah Evans Chukwunnaemeka <okpahevans@gmail.com>
2024-10-25 04:44:14 -0700

    Update userControllers.js

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -4,90 +4,90 @@ import bcrypt from "bcrypt";
 import generateToken from "../utils/generateToken.js";
 
 export const registerUser = TryCatch(async (req, res) => {
-	  const { name, email, password } = req.body;
+  const { name, email, password } = req.body;
 
-	  let user = await User.findOne({ email });
+  let user = await User.findOne({ email });
 
-	  if (user)
-		    return res.status(400).json({
-			          message: "User Already Exists",
-			        });
+  if (user)
+    return res.status(400).json({
+      message: "User Already Exists",
+    });
 
-	  const hashPassword = await bcrypt.hash(password, 10);
+  const hashPassword = await bcrypt.hash(password, 10);
 
-	  user = await User.create({
-		      name,
-		      email,
-		      password: hashPassword,
-		    });
+  user = await User.create({
+    name,
+    email,
+    password: hashPassword,
+  });
 
-	  generateToken(user._id, res);
+  generateToken(user._id, res);
 
-	  res.status(201).json({
-		      user,
-		      message: "User Registered",
-		    });
+  res.status(201).json({
+    user,
+    message: "User Registered",
+  });
 });
 
 export const loginUser = TryCatch(async (req, res) => {
-	  const { email, password } = req.body;
+  const { email, password } = req.body;
 
-	  const user = await User.findOne({ email });
+  const user = await User.findOne({ email });
 
-	  if (!user)
-		    return res.status(400).json({
-			          message: "No User Exist",
-			        });
+  if (!user)
+    return res.status(400).json({
+      message: "No User Exist",
+    });
 
-	  const comparePassword = await bcrypt.compare(password, user.password);
+  const comparePassword = await bcrypt.compare(password, user.password);
 
-	  if (!comparePassword)
-		    return res.status(400).json({
-			          message: "Wrong Password",
-			        });
+  if (!comparePassword)
+    return res.status(400).json({
+      message: "Wrong Password",
+    });
 
-	  generateToken(user._id, res);
+  generateToken(user._id, res);
 
-	  res.status(200).json({
-		      user,
-		      message: "User LoggedIN",
-		    });
+  res.status(200).json({
+    user,
+    message: "User LoggedIN",
+  });
 });
 
 export const myProfile = TryCatch(async (req, res) => {
-	  const user = await User.findById(req.user._id);
+  const user = await User.findById(req.user._id);
 
-	  res.json(user);
+  res.json(user);
 });
 
 export const logoutUser = TryCatch(async (req, res) => {
-	  res.cookie("token", "", { maxAge: 0 });
+  res.cookie("token", "", { maxAge: 0 });
 
-	  res.json({
-		      message: "Logged Out Successfully",
-		    });
+  res.json({
+    message: "Logged Out Successfully",
+  });
 });
 
 export const saveToPlaylist = TryCatch(async (req, res) => {
-	  const user = await User.findById(req.user._id);
+  const user = await User.findById(req.user._id);
 
-	  if (user.playlist.includes(req.params.id)) {
-		      const index = user.playlist.indexOf(req.params.id);
+  if (user.playlist.includes(req.params.id)) {
+    const index = user.playlist.indexOf(req.params.id);
 
-		      user.playlist.splice(index, 1);
+    user.playlist.splice(index, 1);
 
-		      await user.save();
+    await user.save();
 
-		      return res.json({
-			            message: "Removed from playlist",
-			          });
-		    }
+    return res.json({
+      message: "Removed from playlist",
+    });
+  }
 
-	  user.playlist.push(req.params.id);
+  user.playlist.push(req.params.id);
 
-	  await user.save();
+  await user.save();
 
-	  return res.json({
-		      message: "added to playlist",
-		    });
+  return res.json({
+    message: "added to playlist",
+  });
 });
