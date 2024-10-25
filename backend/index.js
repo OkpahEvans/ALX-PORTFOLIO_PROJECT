f1f81a48550098baa3e7b336ce7fe6a9285dcbd6
Okpah Evans Chukwunnaemeka <okpahevans@gmail.com>
2024-10-25 05:16:29 -0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,36 +8,36 @@ import path from "path";
 dotenv.config();
 
 cloudinary.v2.config({
-	  cloud_name: process.env.Cloud_Name,
-	  api_key: process.env.Cloud_Api,
-	  api_secret: process.env.Cloud_Secret,
+  cloud_name: process.env.Cloud_Name,
+  api_key: process.env.Cloud_Api,
+  api_secret: process.env.Cloud_Secret,
 });
 
 const app = express();
 
 // using middlewares
-// app.use(express.json());
-// app.use(cookieParser());
-//
-// const port = process.env.PORT;
-//
-// //importing routes
-// import userRoutes from "./routes/userRoutes.js";
-// import songRoutes from "./routes/songRoutes.js";
-//
-// //using routes
-// app.use("/api/user", userRoutes);
-// app.use("/api/song", songRoutes);
-//
-// const __dirname = path.resolve();
-//
-// app.use(express.static(path.join(__dirname, "/frontend/dist")));
-//
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-//   });
-//
-//   app.listen(port, () => {
-//     console.log(`Server is running on http://localhost:${port}`);
-//       connectDb();
-//       });
+app.use(express.json());
+app.use(cookieParser());
+
+const port = process.env.PORT;
+
+//importing routes
+import userRoutes from "./routes/userRoutes.js";
+import songRoutes from "./routes/songRoutes.js";
+
+//using routes
+app.use("/api/user", userRoutes);
+app.use("/api/song", songRoutes);
+
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+  connectDb();
+});
